7e91a5f8268db9064c425c2e115dba16f7586ef6
thomaslebn <thomas.lebn@protonmail.com>
2021-12-30 17:01:00 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.blueGrey[800],
+    appBar: AppBar(
+      title: const Text("I Am Rich"),
+      backgroundColor: Colors.blueGrey[900],
+    ),
+    body: const Center(
+      child: Image(
+        image: AssetImage('images/diamond.png'),
+      ),
+    ),
+  )));
+}
